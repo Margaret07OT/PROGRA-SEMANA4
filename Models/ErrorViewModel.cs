c0528f328470af49d07243af0df9881836b3fa2b
Margaret07OT <margaret_olivos@usmp.pe>
2022-08-27 10:53:27 -0500

    archivos iniciales

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PROGRA_SEMANA4.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
